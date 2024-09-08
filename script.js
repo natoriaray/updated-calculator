f31ab7dc1e03af55aa1f4e80d8590c768f601279
Natoria Ray <46578731+natoriaray@users.noreply.github.com>
2024-09-07 21:34:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+var calculationDisplay = document.querySelector('.display');
+
+calculationDisplay.value = ''; 
+
+    document.querySelectorAll('#num').forEach( (calButton) => calButton.addEventListener('click', function() {
+    
+        var newVal = calButton.value; 
+
+        calculationDisplay.value = calculationDisplay.value + newVal;
+        console.log(calculationDisplay.value)
+    
+        
+     }))
+
+     document.querySelectorAll('#operator').forEach( (calButton) => calButton.addEventListener('click', function() {
+    
+        console.log('an operator was clicked')
+    
+        
+     }))
 
