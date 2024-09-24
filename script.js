ece75f558ba3df5452a2ae1275aff2b6adb88c7d
Natoria Ray <46578731+natoriaray@users.noreply.github.com>
2024-09-24 10:56:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,29 +7,47 @@ var subClicked;
 var firstVal;
 var nextVal;
 var equalClicked;
+var decimalUsed;
 calculationDisplay.value = ''; 
+
 
 //when button is clicked an action will happen
     document.querySelectorAll('.btn').forEach( (calButton) => calButton.addEventListener('click', function() {
       
       var newVal = calButton.value; 
       
+      if (calButton.value === ".") {
+        decimalUsed = true;
+        calculationDisplay.value = calculationDisplay.value + '.';
+        document.getElementById("decimal").disabled = true;
+        console.log(calculationDisplay.value)
+      }
+      
 //equal sign is pressed
       if (calButton.value === "=") {
         console.log("equal sign was pressed")
         if (addClicked === true) {
           calculationDisplay.value = Number(firstVal) + Number(nextVal);
-        }
-     }  
-      
+        } else if (subClicked === true) {
+          calculationDisplay.value = Number(firstVal) - Number(nextVal);
+        } else if (divClicked === true) {
+          calculationDisplay.value = Number(firstVal) / Number(nextVal);
+        } else if (multiClicked === true) {
+          calculationDisplay.value = Number(firstVal) * Number(nextVal);
+        }             
 //if an initial value is clicked or an operator, an action will happen      
-      if (calButton.id === "num" && operatorClicked === false) {
+      } else if (calButton.id === "num" && operatorClicked === false) {
         calculationDisplay.value = calculationDisplay.value + newVal;
         firstVal = calculationDisplay.value;
-        console.log(firstVal)
-        
+        console.log(firstVal)        
 //get next value if operation has been clicked       
+      } else if (calButton.id === "num" && operatorClicked === true && decimalUsed === true) {
+        document.getElementById("decimal").disabled = false;
+        calculationDisplay.value = calculationDisplay.value + newVal;
+        nextVal = calculationDisplay.value;
+        console.log(nextVal)
       } else if (calButton.id === "num" && operatorClicked === true) {
+        document.getElementById("decimal").disabled = false;
         calculationDisplay.value = '';
         calculationDisplay.value = calculationDisplay.value + newVal;
         nextVal = calculationDisplay.value;
@@ -38,23 +56,29 @@ calculationDisplay.value = '';
       } else if (calButton.id === "operator") {
         console.log('an operator was clicked')
         operatorClicked = true;
+        decimalUsed = false;
  
 //logging which operator is clicked
-        if (calButton.value === "*") {
-          multiClicked = true;
-          return;
-        } else if (calButton.value === "+") {
-          addClicked = true;
-          return;
-        }
-//log if there is a problem        
-      } else {
-        console.log("time to calculate");
-        return;
-      }
+          if (calButton.value === "*") {
+            multiClicked = true;   
+          } else if (calButton.value === "+") {
+            addClicked = true;            
+          } else if (calButton.value === "-") {
+            subClicked = true;            
+          } else if (calButton.value === "/"){
+            divClicked = true;            
+          }
 
-//if equal sign is pressed
-     
-      
-       
+      } else if (calButton.id === "c") {
+        calculationDisplay.value = '';
+        operatorClicked = false;
+        addClicked = false;
+        divClicked = false;
+        multiClicked = false;
+        subClicked = false;
+        firstVal = '';
+        nextVal = '';
+        document.getElementById("decimal").disabled = false;
+      }
     }))
+
