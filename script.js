711b41aa29f3ade3624069a78042c390e4377c0e
Natoria Ray <46578731+natoriaray@users.noreply.github.com>
2024-10-14 16:42:43 -0500

    create new scientific buttons and added functionality to the toggle button

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const operators = ['+', '-', '*', '/'];
 let operatorClicked = false;
 let previousInput = '';
 let nextValue = '';
+let sciClicked = false;
+let sciOn = false;
+const container = document.querySelector(".sciContainer");
 
 calculationDisplay.value = ''; 
 
@@ -68,7 +71,8 @@ const equalClicked = function(prev, next) {
 
 const opClicked = function(opr) {
   if (operatorClicked) {
-    equalClicked(previousInput, nextValue);
+    //equalClicked(previousInput, nextValue); //THIS IS WHAT YOU TURNED OFF. ITS NOT WORKING.
+    //i think you need to create a function that only executes when the the previousvalue is equal to something and the nextValue is equal to '' like what happens at the end of executing the equalClicked function. *basically i need a function that will caluclate the prev and next numbers without the equal sign being involved. Might need to actually calculate based on if an operator is pressed when something is IN previousInput AND nextValue. <--------- That one is the one you want but you can try both.
     operatorClicked = opr.value;
   } else {
     operatorClicked = opr.value;
@@ -80,4 +84,27 @@ const clearDisplay = function() {
   previousInput ='';
   nextValue = '';
   operatorClicked = false;
+};
+
+const sciBtns = [')', '%', 'ln', 'log', 'sq', '(', 'x!', 'e', 'pi', 'ex'];
+
+const chgType = function(arr) {
+  if (sciOn === false) {
+    if (sciClicked === false) {
+      container.style.display = 'grid';
+      arr.forEach(function(i) {
+      const html = `<button type="button" class="styleBtn sciStyle" value="${i}" onclick="btnClicked(this)">${i}</button>`;
+      container.insertAdjacentHTML('beforeend', html)
+      sciClicked = true; 
+      sciOn = true;  
+    })
+    } else {
+      container.style.display = 'grid';
+      sciOn = true;
+    }
+  } else {
+    container.style.display = 'none';
+    sciOn = false;
+  };
+  
 };
